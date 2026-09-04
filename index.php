47236aaacdf2d4bd8581eba9eb84bcd22186eea7
salmanshariarmy <salmanshariardipu@gmail.com>
2026-09-04 18:39:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Replace with your actual Telegram Bot Token from BotFather
-define('BOT_TOKEN', 'YOUR_TELEGRAM_BOT_TOKEN');
+define('BOT_TOKEN', '8964814604:AAFpE9qnn4XBCfaEWBnfGEVp_Mpa72LmRzg');
 
 // Get the incoming update from Telegram
 $content = file_get_contents("php://input");
